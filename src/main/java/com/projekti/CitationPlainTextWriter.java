f16371b5a65dd8cf8ea6693a4d6612c48cf5248e
Pekka <peltirasia.xd@gmail.com>
2024-12-01 19:32:36 +0200

    added classes for writing plain and bibtex files

---
+++ src/main/java/com/projekti/CitationPlainTextWriter.java
@@ -0,0 +1,23 @@
+package com.projekti;
+
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.util.List;
+
+public class CitationPlainTextWriter {
+
+    public void WriteToFile(List<Citation> citations, String filename) {
+        try (PrintWriter writer = new PrintWriter(filename, "UTF-8")) {
+            for(Citation c : citations) {
+                writer.println(c.toString());
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+}
