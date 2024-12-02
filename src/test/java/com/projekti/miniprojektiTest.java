977c5b54d7110a1fecdf5d62cf5cbf76253c9db1
Viianen Noe <noe.n.viianen@student.jyu.fi>
2024-12-02 19:47:54 +0200

    Added first test for main

--- src/test/java/com/projekti/miniprojektiTest.java
+++ src/test/java/com/projekti/miniprojektiTest.java
@@ -4,6 +4,12 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.Map;
+import java.util.Scanner;
+
 public class miniprojektiTest {
 
     @Test
@@ -18,6 +24,27 @@ public class miniprojektiTest {
         Miniprojekti calculator = new Miniprojekti();
         int result = calculator.subtract(5, 2);
         assertEquals(3, result);
+    }
+    
+    @Test
+    public void testMainQuitImmediately() {
+        String userInput = "-1" + System.getProperty("line.separator");
+        ByteArrayInputStream in = new ByteArrayInputStream(userInput.getBytes());
+        System.setIn(in);
+        ByteArrayOutputStream os = new ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream(os);
+        System.setOut(printStream);
+        Miniprojekti.main(null);
+        String actual = os.toString();
+        String expected = "-1 TO QUIT!\r\n"
+                + "Give a type:\r\n"
+                + "0: Inproceedings\r\n"
+                + "1: Article\r\n"
+                + "2: Book\r\n"
+                + "Quitting!\n"
+                + "\r\n"
+                + "No citations were added\r\n";
+        assertEquals(expected, actual);
     }
 
     // ... more test cases for different scenarios
