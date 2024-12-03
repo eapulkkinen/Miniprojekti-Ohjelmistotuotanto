c5eaee38693c5208fece2dd6d223116fb42ad226
eapulkkinen <epulkkinena@gmail.com>
2024-12-03 11:41:21 +0200

    Uusi testi

--- src/test/java/com/projekti/miniprojektiTest.java
+++ src/test/java/com/projekti/miniprojektiTest.java
@@ -32,6 +32,32 @@ public class miniprojektiTest {
                 + "No citations were added" + System.getProperty("line.separator");
         assertEquals(expected, actual);
     }
-
-    // ... more test cases for different scenarios
+    
+    @Test
+    public void testMainInvalidTypeNumber() {
+    	String testNumber = "5";
+    	String userInput = testNumber + System.getProperty("line.separator") + "-1" + System.getProperty("line.separator");
+        ByteArrayInputStream in = new ByteArrayInputStream(userInput.getBytes());
+        System.setIn(in);
+        ByteArrayOutputStream os = new ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream(os);
+        System.setOut(printStream);
+        Miniprojekti.main(null);
+        String actual = os.toString();
+        String expected = "-1 TO QUIT!" + System.getProperty("line.separator")
+                + "Give a type:" + System.getProperty("line.separator")
+                + "0: Inproceedings" + System.getProperty("line.separator")
+                + "1: Article" + System.getProperty("line.separator")
+                + "2: Book" + System.getProperty("line.separator")
+            	+ "Not a valid type! " + testNumber + System.getProperty("line.separator")
+            	+ "-1 TO QUIT!" + System.getProperty("line.separator")
+                + "Give a type:" + System.getProperty("line.separator")
+                + "0: Inproceedings" + System.getProperty("line.separator")
+                + "1: Article" + System.getProperty("line.separator")
+                + "2: Book" + System.getProperty("line.separator")
+                + "Quitting!" + System.getProperty("line.separator")
+                + System.getProperty("line.separator")
+                + "No citations were added" + System.getProperty("line.separator");
+        assertEquals(expected, actual);
+    }
 }
