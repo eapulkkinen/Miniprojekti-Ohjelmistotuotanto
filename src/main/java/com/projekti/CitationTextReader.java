68c0eb42277ce714812a2e554e2d8e0f1742f41f
Marius <marius.o.peltonen@student.jyu.fi>
2024-12-03 11:19:29 +0200

    Lisättiin virheidenkäsittelijät

--- src/main/java/com/projekti/CitationTextReader.java
+++ src/main/java/com/projekti/CitationTextReader.java
@@ -2,14 +2,25 @@ package com.projekti;
 
 import java.util.Scanner;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.UnsupportedEncodingException;
 
 public class CitationTextReader{
     
     public static void ReadFromTextFile(){
-        Scanner input = new Scanner(new File("UTF-8"));
-        while (input.hasNextLine()){
-            System.out.println(input.nextLine());
+        try (Scanner input = new Scanner(new File("UTF-8"))){
+            while (input.hasNextLine()){
+                System.out.println(input.nextLine());
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            throw e;
         }
+       
+        
         
 
     }
