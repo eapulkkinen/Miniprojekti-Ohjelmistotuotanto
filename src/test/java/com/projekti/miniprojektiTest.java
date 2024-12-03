8154386a209f35f1b92827b6696bed56f6ba4c2b
eapulkkinen <epulkkinena@gmail.com>
2024-12-03 10:09:53 +0200

    Testiohjelman poisto

--- src/test/java/com/projekti/miniprojektiTest.java
+++ src/test/java/com/projekti/miniprojektiTest.java
@@ -13,20 +13,6 @@ import java.util.Scanner;
 public class miniprojektiTest {
 
     @Test
-    public void testAddPositiveNumbers() {
-        Miniprojekti calculator = new Miniprojekti();
-        int result = calculator.add(2, 3);
-        assertEquals(5, result);
-    }
-
-    @Test
-    public void testSubtractPositiveNumbers() {
-        Miniprojekti calculator = new Miniprojekti();
-        int result = calculator.subtract(5, 2);
-        assertEquals(3, result);
-    }
-    
-    @Test
     public void testMainQuitImmediately() {
         String userInput = "-1" + System.getProperty("line.separator");
         ByteArrayInputStream in = new ByteArrayInputStream(userInput.getBytes());
