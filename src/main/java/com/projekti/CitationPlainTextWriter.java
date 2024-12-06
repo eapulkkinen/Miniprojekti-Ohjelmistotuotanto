1276140ea7734c30ae03141449ae91a8172cc2d5
eapulkkinen <epulkkinena@gmail.com>
2024-12-06 15:25:14 +0200

    Checkstyle testi

--- src/main/java/com/projekti/CitationPlainTextWriter.java
+++ src/main/java/com/projekti/CitationPlainTextWriter.java
@@ -9,24 +9,24 @@ import java.util.List;
  * Class for writing a text file based on given citations.
  */
 public class CitationPlainTextWriter {
-  
-  /**
-   * Writes citations given by the user to a text file.
 
-   * @param citations a list of citations
-   * @param filename name for the new file
-   */
-  public static void writeToFile(List<Citation> citations, String filename) {
-    try (PrintWriter writer = new PrintWriter(filename, "UTF-8")) {
-      for (Citation c : citations) {
-        writer.println(c.toString());
-      } 
-    } catch (FileNotFoundException e) {
-      e.printStackTrace();
-    } catch (UnsupportedEncodingException e) {
-      e.printStackTrace();
-    } catch (Exception e) {
-      throw e;
-    }
-  }
+	/**
+	 * Writes citations given by the user to a text file.
+	 * @param citations a list of citations
+	 * @param filename name for the new file
+	 */
+	public static void writeToFile(final List<Citation> citations,
+			final String filename) {
+		try (PrintWriter writer = new PrintWriter(filename, "UTF-8")) {
+			for (Citation c : citations) {
+				writer.println(c.toString());
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (UnsupportedEncodingException e) {
+	      e.printStackTrace();
+	    } catch (Exception e) {
+	      throw e;
+	    }
+	}
 }
