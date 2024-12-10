1e70ef1037d141b98698b4531437b45bd20f8d54
Marius <marius.o.peltonen@student.jyu.fi>
2024-12-10 18:43:28 +0200

    Testattiin korjata

--- src/test/java/com/projekti/CitationPlainTextWriter.java
+++ src/test/java/com/projekti/CitationPlainTextWriter.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-public class citationWriterTest {
+public class CitationPlainTextWriterTest {
 
     @Test
     public void testFileNotFoundException() throws FileNotFoundException {
